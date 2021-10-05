29fe24fff6dcaf556e5f084a86cef5684c7a3965
Atthiyyath <56459274+Atthiyya22194@users.noreply.github.com>
2021-10-06 02:25:34 +0700

    Add files via upload

---
+++ AnalyticsManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase.Analytics;
+
+public class AnalyticsManager
+{
+    private static void LogEvent(string eventName, params Parameter[] parameters)
+    {
+        //method utama menambahkan firebase
+        FirebaseAnalytics.LogEvent(eventName, parameters);
+    }
+
+    public static void LogUpgradeEvent(int resourcesIndex, int level)
+    {
+        LogEvent(
+            FirebaseAnalytics.EventLevelUp,
+            new Parameter(FirebaseAnalytics.ParameterIndex, resourcesIndex.ToString()),
+            new Parameter(FirebaseAnalytics.ParameterLevel, level)
+            );
+        //resourceIndex digunakan sbg ID, karena itulah tipe datanya string bukan int
+    }
+
+    public static void LogUnlockEvent(int resourceIndex)
+    {
+        LogEvent(
+            FirebaseAnalytics.EventUnlockAchievement,
+            new Parameter(FirebaseAnalytics.ParameterIndex, resourceIndex.ToString())
+            );
+    }
+
+    public static void SetUserProperties(string name, string value)
+    {
+        FirebaseAnalytics.SetUserProperty(name, value);
+    }
+}
